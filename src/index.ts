a8282d57dc88861a6d2dbd35fbbb619cf61464e6
Jonathan Wood <jonathan.m.wood95@gmail.com>
2021-10-02 16:31:49 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {LifeWidget} from './life_widget.js';
+let body = <HTMLBodyElement>document.getElementsByTagName("body")[0];
+let lifeWidget = new LifeWidget();
+body.appendChild(lifeWidget.createHtml());
